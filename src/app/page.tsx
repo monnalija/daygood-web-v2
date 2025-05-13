6cbe9dbd7ce02a7268de15295e975ec1e0d8630a
woosung <monnalija@naver.com>
2025-05-13 17:52:11 +0900

    Feat: weather-icon 직접 임포트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import { useEffect, useState } from "react";
 import { getCurrentWeather, getOneHour } from "./actions";
 import { cn } from "@/lib/utils";
@@ -55,42 +54,74 @@ export default function Home() {
 
     if (pty > 0) {
       if (pty === 1) {
-        return { name: "비", icon: "wi-day-rain" };
+        return { name: "비", icon: "wi-day-rain", color: "text-blue-400" };
       } else if (pty === 2) {
-        return { name: "비/눈", icon: "wi-day-rain-mix" };
+        return {
+          name: "비/눈",
+          icon: "wi-day-rain-mix",
+          color: "text-blue-300",
+        };
       } else if (pty === 3) {
-        return { name: "눈", icon: "wi-day-snow" };
+        return { name: "눈", icon: "wi-day-snow", color: "text-blue-200" };
       } else if (pty === 5) {
-        return { name: "빗방울", icon: "wi-day-hail" };
+        return { name: "빗방울", icon: "wi-day-hail", color: "text-blue-400" };
       } else if (pty === 6) {
-        return { name: "빗방울눈날림", icon: "wi-day-sleet" };
+        return {
+          name: "빗방울눈날림",
+          icon: "wi-day-sleet",
+          color: "text-blue-400",
+        };
       } else if (pty === 7) {
-        return { name: "눈날림", icon: "wi-day-snow-wind" };
+        return {
+          name: "눈날림",
+          icon: "wi-day-snow-wind",
+          color: "text-blue-200",
+        };
       }
     } else {
       if (sky === 1) {
-        return { name: "맑음", icon: "wi-day-sunny" };
+        return { name: "맑음", icon: "wi-day-sunny", color: "text-yellow-300" };
       } else if (sky === 3) {
-        return { name: "구름많음", icon: "wi-day-cloud" };
+        return {
+          name: "구름많음",
+          icon: "wi-day-cloudy",
+          color: "text-gray-400",
+        };
       } else if (sky === 4) {
-        return { name: "흐림", icon: "wi-day-cloud" };
+        return { name: "흐림", icon: "wi-day-cloudy", color: "text-gray-400" };
       }
     }
   };
 
   return (
     <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="flex flex-col items-center justify-center ">
-        <div className="flex text-6xl w-23 h-23 justify-center items-center">
-          <i
-            className={cn(
-              "wi",
-              checkSky(currentWeather?.PTY, oneHourWeather?.SKY)?.icon
-            )}
-          ></i>
+      <div className="flex flex-row items-center gap-3">
+        <div className="flex flex-col items-center justify-center ">
+          <div className="flex text-6xl w-22 h-22 justify-center items-center">
+            <i
+              className={cn(
+                "wi",
+                checkSky(currentWeather?.PTY, oneHourWeather?.SKY)?.icon,
+                checkSky(currentWeather?.PTY, oneHourWeather?.SKY)?.color
+              )}
+            ></i>
+          </div>
+          <p>{checkSky(currentWeather?.PTY, oneHourWeather?.SKY)?.name}</p>
         </div>
-        <p>{checkSky(currentWeather?.PTY, oneHourWeather?.SKY)?.name}</p>
-        <p>{currentWeather?.T1H}°C</p>
+
+        <div className="flex flex-col items-center justify-center">
+          <p className="text-3xl mb-1">{currentWeather?.T1H}°C</p>
+          <div className="flex flex-col items-center justify-center">
+            <div className="flex flex-row items-center justify-center gap-1">
+              <i className="wi wi-umbrella text-md text-blue-700"></i>
+              <p className="text-xl">{currentWeather?.RN1}mm</p>
+            </div>
+            <div className="flex flex-row items-center justify-center gap-1">
+              <i className="wi wi-humidity text-md text-sky-500"></i>
+              <p className="text-xl">{currentWeather?.REH}%</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,6 +1,2 @@
 export const API_URL = "https://us-central1-daygood-4d579.cloudfunctions.net";
-
-// export const API_URL = Platform.select({
-//   ios: 'http://localhost:5001/daygood-4d579/us-central1',
-//   android: 'http://10.0.2.2:5001/daygood-4d579/us-central1',
-// })
+// export const API_URL = "http://127.0.0.1:5001/daygood-4d579/us-central1";
